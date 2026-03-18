06b6e854be89bf26dd9cd9b4946db58cfab5bf40
camilohl24 <juancamilohernandezhl@gmail.com>
2026-03-18 18:18:48 -0500

    creation of owner and mechanic controllers

---
+++ RideManager.Api/Controllers/MechanicsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RideManager.Api.Data;
+using RideManager.Api.Models;
+
+namespace RideManager.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MechanicsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public MechanicsController(AppDbContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Mechanic>>> GetMechanics()
+    {
+        return await _context.Mechanics.ToListAsync();
+    }
+    [HttpPost]
+    public async Task<ActionResult<Mechanic>> CreateMechanic(Mechanic mechanic)
+    {
+        _context.Mechanics.Add(mechanic);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetMechanics), new { id = mechanic.Id }, mechanic);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Mechanic>> GetMechanic(int id)
+    {
+        var mechanic = await _context.Mechanics.FindAsync(id);
+        if (mechanic == null)
+        {
+            return NotFound();
+        }
+        return mechanic;
+    }
+
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateMechanic(int id, Mechanic mechanic)
+    {
+        if (id != mechanic.Id)
+            return BadRequest();
+        _context.Entry(mechanic).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
+
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteMechanic(int id)
+    {
+        var mechanic = await _context.Mechanics.FindAsync(id);
+        if (mechanic == null)
+            return NotFound();
+        _context.Mechanics.Remove(mechanic);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+}
---
+++ RideManager.Api/Controllers/OwnersController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RideManager.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using RideManager.Api.Data;
+
+namespace RideManager.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OwnersController : ControllerBase
+{
+
+    private readonly AppDbContext _context;
+
+    public OwnersController(AppDbContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Owner>>> GetOwners()
+    {
+        return await _context.Owners
+            .Include(o => o.Motorcycles)
+            .ToListAsync();
+    }
+    [HttpPost]
+    public async Task<ActionResult<Owner>> CreateOwner(Owner owner)
+    {
+        _context.Owners.Add(owner);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetOwners), new { id = owner.Id }, owner);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Owner>> GetOwner(int id)
+    {
+        var owner = await _context.Owners
+            .Include(o => o.Motorcycles)
+            .FirstOrDefaultAsync(o => o.Id == id);
+        if (owner == null)
+            return NotFound();
+        return owner;
+    }
+
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateOwner (int id, Owner owner)
+    {
+        if(id != owner.Id)
+           return BadRequest();
+        _context.Entry(owner).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
+        
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteOwner(int id)
+    {
+        var owner = await _context.Owners.FindAsync(id);
+        if (owner == null)
+            return NotFound();
+        _context.Owners.Remove(owner);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
