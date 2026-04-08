e86f7018aa34eb8c224e327b8d634cd497abf033
camilohl24 <juancamilohernandezhl@gmail.com>
2026-04-08 18:08:41 -0500

    feat: add DashboardPage with services and state

--- RideManager.Web/src/pages/dashboard/DashboardPage.tsx
+++ RideManager.Web/src/pages/dashboard/DashboardPage.tsx
@@ -1,13 +1,46 @@
-import { useEffect, useState } from "react";
-import { useAuthStore } from "@/store/authStore";
-import axios from "axios";
-
-
+import { getAppointments } from '@/services/appointmentService';
+import { getWorkOrders } from '@/services/workOrderService';
+import { getOwners } from '@/services/ownerService';
+import { getMechanics } from '@/services/mechanicService';
+import { useEffect, useState } from 'react';
+import { type MechanicResponse, type AppointmentResponse, type OwnerResponse, type WorkOrderResponse } from '@/types/api';
+import { WorkOrderStatus } from '@/types/enums';
 
 export default function DashboardPage() {
-    return (
-        <div className="text-white">
-            <h1>Dashboard</h1>
-        </div>
-    )
+    const [workOrders, setWorkOrders] = useState<WorkOrderResponse[]>([]);
+    const [owners, setOwners] = useState<OwnerResponse[]>([]);
+    const [mechanics, setMechanics] = useState<MechanicResponse[]>([]);
+    const [appointments, setAppointments] = useState<AppointmentResponse[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        async function fetchData() {
+
+            try {
+                const workOrders = await getWorkOrders()
+                const appointments = await getAppointments()
+                const owners = await getOwners()
+                const mechanics = await getMechanics()
+                setWorkOrders(workOrders)
+                setAppointments(appointments)
+                setOwners(owners)
+                setMechanics(mechanics)
+            } catch (error) {
+                console.error('Error al cargar datos:', error)
+            } finally {
+                setLoading(false)
+            }
+
+        }
+        fetchData()
+
+
+    }, [])
+    const activeOrders = workOrders.filter(o => 
+        o.status !== WorkOrderStatus.Done &&
+        o.status !== WorkOrderStatus.ReadyForDelivery
+    ).length
+
 }
+
+
